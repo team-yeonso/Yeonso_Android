3589f29903842d3fdb73e653ed500b4e37d4a48c
Sohyeon <sohyeonpark1210@gmail.com>
2018-04-24 23:56:04 +0900

    Add MainActivity and Navigation drawer

--- app/src/main/java/dsm/yeonso/MainActivity.java
+++ app/src/main/java/dsm/yeonso/MainActivity.java
@@ -1,12 +1,22 @@
 package dsm.yeonso;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import android.content.ClipData;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.style.TextAppearanceSpan;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private MainFragmentViewPager mainFragmentViewPager;
     private TabLayout tabLayout;
@@ -17,8 +27,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tabLayout = (TabLayout) findViewById(R.id.mainActivity_tabLayout);
-        viewPager = (ViewPager) findViewById(R.id.mainActivity_viewPager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout) findViewById(R.id.content_main2_tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.content_main2_viewPager);
 
         mainFragmentViewPager = new MainFragmentViewPager(getSupportFragmentManager(),tabLayout.getTabCount());
         viewPager.setAdapter(mainFragmentViewPager);
@@ -40,5 +53,51 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // TODO: 2018-04-24 글씨 색 바꾸기 
+        Menu menu = navigationView.getMenu();
+        MenuItem menuItem= menu.findItem(R.id.nav_camera);
+        SpannableString s = new SpannableString(menuItem.getTitle());
+        s.setSpan(new TextAppearanceSpan(this, R.style.Navigation),0,s.length(),0);
+        menuItem.setTitle(s);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } 
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
