47da7d7ad68592aa491995349f9af45a16726560
Sohyeon <sohyeonpark1210@gmail.com>
2018-05-01 23:35:27 +0900

    Add find job

--- app/src/main/java/dsm/yeonso/MainTabFragment2.java
+++ app/src/main/java/dsm/yeonso/MainTabFragment2.java
@@ -1,6 +1,7 @@
 package dsm.yeonso;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -17,27 +18,26 @@ import java.lang.reflect.Array;
  * A simple {@link Fragment} subclass.
  */
 public class MainTabFragment2 extends Fragment {
+    private Spinner jobFirst, jobSecond;
     private ArrayAdapter<CharSequence> jobFirstAdapter, jobSecondAdapter;
-    private Spinner jobFirst;
-    private Spinner jobSecond;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.main_tab_fragment_2,container,false);
+        View view = inflater.inflate(R.layout.main_tab_fragment_2, container, false);
 
         jobFirst = (Spinner) view.findViewById(R.id.mainTabFragment_job_first);
         jobSecond = (Spinner) view.findViewById(R.id.mainTabFragment_job_second);
 
-        jobFirstAdapter = ArrayAdapter.createFromResource(getContext(), R.array.job_first,android.R.layout.simple_spinner_dropdown_item);
+        jobFirstAdapter = ArrayAdapter.createFromResource(getContext(), R.array.job_first, android.R.layout.simple_spinner_item);
         jobFirst.setAdapter(jobFirstAdapter);
 
         jobFirst.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if(jobFirst.getItemAtPosition(i).equals("전체")) {
-                    jobSecondAdapter = ArrayAdapter.createFromResource(getContext(), R.array.job_second_all,android.R.layout.simple_spinner_dropdown_item);
-                    jobSecondAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                if (jobFirst.getItemAtPosition(i).equals("전체")) {
+                    jobSecondAdapter = ArrayAdapter.createFromResource(getContext(), R.array.job_second_all, android.R.layout.simple_spinner_item);
+                    jobSecondAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
                     jobSecond.setAdapter(jobSecondAdapter);
                     jobSecond.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                         @Override
@@ -50,9 +50,9 @@ public class MainTabFragment2 extends Fragment {
                             // 아무것도 선택이 안됐을때
                         }
                     });
-                } else if(jobFirst.getItemAtPosition(i).equals("일반")) {
-                    jobSecondAdapter = ArrayAdapter.createFromResource(getContext(), R.array.job_second_general,android.R.layout.simple_spinner_dropdown_item);
-                    jobSecondAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                } else if (jobFirst.getItemAtPosition(i).equals("일반")) {
+                    jobSecondAdapter = ArrayAdapter.createFromResource(getContext(), R.array.job_second_general,android.R.layout.simple_spinner_item);
+                    jobSecondAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
                     jobSecond.setAdapter(jobSecondAdapter);
                     jobSecond.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                         @Override
