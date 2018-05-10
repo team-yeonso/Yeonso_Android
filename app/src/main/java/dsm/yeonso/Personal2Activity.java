bf82f9b88965c898160ed3ef72e82696f64d6dbd
Sohyeon <sohyeonpark1210@gmail.com>
2018-05-10 13:10:03 +0900

    Rename activity

---
+++ app/src/main/java/dsm/yeonso/Personal2Activity.java
@@ -0,0 +1,65 @@
+package dsm.yeonso;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class Personal2Activity extends AppCompatActivity {
+
+    private Intent intent;
+    private DatePicker datePicker;
+    private TextView textView;
+    private ImageButton before;
+    private ImageButton next;
+    private int age;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal2);
+
+        datePicker = findViewById(R.id.birthActivity_datePicker);
+        textView = findViewById(R.id.birthActivity_textView_age);
+        before = findViewById(R.id.birthActivity_imageButton_before);
+        next = findViewById(R.id.birthActivity_imageButton_next);
+
+        textView.setVisibility(View.GONE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+                @Override
+                public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
+                    Calendar current = Calendar.getInstance();
+                    int currentYear = current.get(Calendar.YEAR);
+                    int currentMonth = current.get(Calendar.MONTH);
+                    int currentDay = current.get(Calendar.DAY_OF_MONTH);
+                    int birthYear = datePicker.getYear();
+                    age = currentYear - birthYear;
+                    if (birthYear * 100 + birthYear > currentMonth * 100 + currentDay) {
+                        age--;
+                    }
+
+                    textView.setText("만 " + age + "세 입니다.");
+                    textView.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+
+        next.setOnClickListener(v -> {
+            if (age < 18) {
+                Snackbar.make(v, "만 18세 이상부터 사용이 가능합니다.",Snackbar.LENGTH_SHORT).show();
+            } else {
+                intent = new Intent(getApplicationContext(), Personal3Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
